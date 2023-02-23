81689476e7244a616c1450af9e19048f5736c50c
kishor G <65946231+kishorsumathi@users.noreply.github.com>
2023-02-23 20:09:00 +0530

    Update __main__.py

--- api/packages/authentication/validation/__main__.py
+++ api/packages/authentication/validation/__main__.py
@@ -2,13 +2,15 @@ import os
 
 def main(req):
     default_api=os.environ.get('default_api')
+    success=os.environ.get("for_true")
+    error=os.environ.get("for_false")
     api_key=req.get("api_key")
     try:
         if str(api_key)==str(default_api):
-            return {"body":"True"}
+            return {"body":str(success)}
         else:
-            return {"body":"False"}
+            return {"body":str(error)}
     except:
-        return {"body":"False"}
+        return {"body":str(error)}
 
     
